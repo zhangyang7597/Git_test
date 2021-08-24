d54a40d79a0bbb4ac4ebd2efe6e7c68c7c891e25
zy <756788535@qq.com>
2021-08-24 20:28:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/MainApp.java
@@ -0,0 +1,18 @@
+package com.atguigu;
+
+import org.apache.camel.main.Main;
+
+/**
+ * A Camel Application
+ */
+public class MainApp {
+
+    /**
+     * A main() so we can easily run these routing rules in our IDE
+     */
+    public static void main(String... args) throws Exception {
+        System.out.println("test");
+    }
+
+}
+
